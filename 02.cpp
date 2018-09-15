28aa761c2953f6118434ba8fde7b83b770fb9185
Darldil <darldil@users.noreply.github.com>
2018-09-15 13:03:25 +0200

    Primer commit
    Primer commit.
    01 - 02

---
+++ 02.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+std::string transformar(unsigned int a) {
+
+	if (a == 1) return "1";
+	return transformar(a / 2) + std::to_string(a%2);
+}
+
+void resuelveCaso() {
+	unsigned int a;
+	std::cin >> a;
+
+	std::cout << transformar(a) << '\n';
+}
+
+int main() {
+	int casos = 0;
+	std::cin >> casos;
+
+	for (int i = 0; i < casos; ++i) {
+		resuelveCaso();
+	}
+	return 0;
+}
