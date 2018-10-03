75e0a6ee43cc9cc747d3ecf72dd4cc4a09864add
Mauricio Abbati <darldil@users.noreply.github.com>
2018-10-03 15:30:54 +0200

    Ejercicio 09 - Divide y vencerás

--- 09.cpp
+++ 09.cpp
@@ -2,29 +2,37 @@
 #include <vector>
 #include <fstream>
 
-//		| c0 si n = impar
+//		| c0 si n == 1
 // T(n) |
-//		| T(n + 1) + c0 si n = par
+//		| T(n/2) + c0 si n > 1
 //
 //	Despliegue:
-//	T(n) = T(n+1) + c0 = T(n+2) + c0 + c0 = T(n+3) + c0 + c0 + c0 =...
-//	= T(k+n) + k*c0
-//	Por tanto, si k = impar --> T(c0) + n*c0 = c0 + n*c0
+//	T(n) = T(n/2) + c0 = T(n/(2^2)) + c0 + c0 = T(n/(2^3)) + c0 + c0 + c0 =...
+//	= T(n/(2^k)) + k*c0
+//	Por tanto, log(n) + n*c0
 //
-//Coste O(n)
-unsigned int buscarImpar(const std::vector<unsigned int> &v, const int &pos) {
-	if (pos == v.size()) // vector de 1 elemento 
-		return v[pos]; 
-	else {
-		if (v[pos]%2 != 0) return v[pos];
-		return buscarImpar(v, pos + 1);
-	}
+//Coste O(logn)
+unsigned int buscarImpar(const std::vector<unsigned int> &v, const int &ini, const int &fin) {
+	if (ini + 1 == fin) // Si el vector tiene un elemento
+		return v[ini];
+
+	int m = (ini + fin - 1) / 2;
+
+	if (v[m] % 2 != 0) return v[m];
+	unsigned int sum = 0;
+
+	for (int i = 0; i < m; ++i)
+		sum += v[i];
+
+	if (sum % 2 != 0) 
+		return buscarImpar(v, ini, m + 1);
+	else
+		return buscarImpar(v, m + 1, fin);
 }
 
 bool resuelveCaso() {
 
 	unsigned int n;
-
 	std::cin >> n;
 
 	if (n == 0) return false;
@@ -37,7 +45,7 @@ bool resuelveCaso() {
 		v.push_back(aux);
 	}
 
-	std::cout << buscarImpar(v, 0) << '\n';
+	std::cout << buscarImpar(v, 0, v.size()) << '\n';
 
 	return true;
 
@@ -46,7 +54,7 @@ bool resuelveCaso() {
 int main() {
 	// Para la entrada por fichero.
 	// Comentar para acepta el reto
-	#ifndef DOMJUDGE
+	/*#ifndef DOMJUDGE
 		std::ifstream in("datos.txt");
 		auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
 	#endif */
@@ -55,10 +63,10 @@ int main() {
 
 
 		// Para restablecer entrada. Comentar para acepta el reto
-	#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	/*#ifndef DOMJUDGE // para dejar todo como estaba al principio
 		std::cin.rdbuf(cinbuf);
 		system("PAUSE");
-	#endif
+	#endif*/
 
 	return 0;
 }
