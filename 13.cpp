79a6132b31c53f17ecd3bce148b1ec39d2e1af71
Mauricio Abbati <darldil@users.noreply.github.com>
2018-10-08 11:57:26 +0200

    Ejercicio 13 - Divide y vencerás
    Corregido

--- 13.cpp
+++ 13.cpp
@@ -1,5 +1,5 @@
-//Ejercicio 12
-//Mauricio Abbati Loureiro - EDA 2ÂºE 2018/2019
+//Ejercicio 13
+//Mauricio Abbati Loureiro - EDA 2ºE 2018/2019
 //E01
 #include <iostream>
 #include <vector>
@@ -17,13 +17,13 @@ struct par {
 //        | 2T(n/2) + c0 si n > 1
 //
 //    Despliegue:
-//    T(n) = 2T(n/2) + c0 = 2^2T(n/(2^2)) + c0 + c0 = 2^3T(n/(2^3)) + c0 + c0 + c0 =...
-//    = 2^kT(n/(2^k)) + k*c0 --> 2^log(n)T(1) + log(n) * c0 
+//    T(n) = 2T(n/2) + c0 = 2^2T(n/(2^2)) + 2c0 + c0 = 2^3T(n/(2^3)) + 2^2c0 + c0 =...
+//    = 2^kT(n/(2^k)) + 2*kc0 + 2^(k-1)c0 + 2^(k-2)c0 + ... + 2^(k-n)c0 
 //
 //Coste O(n)
 par dividir(const std::vector<int> &v, const int &ini, const int &fin) {
 	if (ini + 1 >= fin - 1) { //Vector de dos elementos
-		if (v[ini] <= v[fin - 1]) return{ v[ini], v[fin - 1], true};
+		if (v[ini] <= v[fin - 1]) return{ v[ini], v[fin - 1], true };
 		else return { v[fin - 1], v[ini], false };
 	}
 
@@ -43,7 +43,7 @@ par dividir(const std::vector<int> &v, const int &ini, const int &fin) {
 		p.ord = false;
 		return p;
 	}
-	p.ord = iz.ord && dr.ord && iz.mayor < dr.mayor && iz.menor < dr.menor;
+	p.ord = iz.mayor <= dr.mayor && iz.menor <= dr.menor;
 
 	p.menor = std::min(iz.menor, dr.menor);
 	p.mayor = std::max(iz.mayor, dr.mayor);
@@ -60,9 +60,14 @@ bool resuelveCaso() {
 
 	std::vector<int> v;
 
-	while (n != 0){
+	while (n != 0) {
 		v.push_back(n);
 		std::cin >> n;
+	}
+
+	if (v.size() == 1) {
+		std::cout << "SI\n";
+		return true;
 	}
 
 	par iz = dividir(v, 0, v.size() / 2);
@@ -78,19 +83,19 @@ bool resuelveCaso() {
 int main() {
 	// Para la entrada por fichero.
 	// Comentar para acepta el reto
-	#ifndef DOMJUDGE
+#ifndef DOMJUDGE
 	std::ifstream in("datos.txt");
 	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
-	#endif
+#endif
 
 	while (resuelveCaso());
 
 
 	// Para restablecer entrada. Comentar para acepta el reto
-	#ifndef DOMJUDGE // para dejar todo como estaba al principio
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
 	std::cin.rdbuf(cinbuf);
 	system("PAUSE");
-	#endif
+#endif
 
 	return 0;
 }
