a66ccc5d5528cdcc9ca54f950a25b50035670364
Mauricio Abbati <darldil@users.noreply.github.com>
2019-02-02 18:25:33 +0100

    Ejercicio 02
    Mejoradas implementaciones.

--- Cuatrimestre 2/Ejercicio 2/Horas.h
+++ Cuatrimestre 2/Ejercicio 2/Horas.h
@@ -10,6 +10,7 @@
 #define Horas_h
 
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <stdexcept>
 #include <string>
@@ -55,30 +56,10 @@ public:
 };
 
 inline std::ostream& operator <<(std::ostream &out , Horas const& h) {
-    std::string hora, minutos, segundos;
+    out << std::setfill('0') << std::setw(2) << h.getHora() << ":" <<
+    std::setfill('0') << std::setw(2) << h.getMinutos() << ":" <<
+    std::setfill('0') << std::setw(2) << h.getSegundos();
     
-    if (h.getHora() == 0) hora = "00";
-    else if (h.getHora() < 10) {
-        hora = "0";
-        hora += std::to_string(h.getHora());
-    }
-    else hora = std::to_string(h.getHora());
-    
-    if (h.getMinutos() == 0) minutos = "00";
-    else if (h.getMinutos() < 10) {
-        minutos = "0";
-        minutos += std::to_string(h.getMinutos());
-    }
-    else minutos = std::to_string(h.getMinutos());
-    
-    if (h.getSegundos() == 0) segundos = "00";
-    else if (h.getSegundos() < 10) {
-        segundos = "0";
-        segundos += std::to_string(h.getSegundos());
-    }
-    else segundos = std::to_string(h.getSegundos());
-    
-    out << hora << ":" << minutos << ":" << segundos;
     return out;
 }
 
@@ -88,8 +69,7 @@ inline std::istream& operator >>(std:: istream &in , Horas &hora) {
     
     in >> h >> aux >> m >> aux >> s;
     
-    Horas sol(h, m, s);
-    hora = sol;
+    hora = Horas(h, m, s);
     
     return in;
 }
