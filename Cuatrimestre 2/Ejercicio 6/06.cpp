84c5b911d42ea93ae64c98a3a9cf1363cf03edb9
Mauricio Abbati <darldil@users.noreply.github.com>
2019-02-13 23:01:10 +0100

    Ejercicio 06
    Mejorada implementación.

--- Cuatrimestre 2/Ejercicio 6/06.cpp
+++ Cuatrimestre 2/Ejercicio 6/06.cpp
@@ -1,90 +1,74 @@
 //Ejercicio 06
-//Mauricio Abbati Loureiro - EDA 2ÂºE 2018/2019
+//Mauricio Abbati Loureiro - EDA 2ºE 2018/2019
 //E01
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <stack>
-#include <vector>
 
-class ParentesisEquilibrados {
-    std::stack<char> pila;
-    bool equilibrado;
-    
-public:
-    ParentesisEquilibrados(): equilibrado(true) {};
-    
-    bool getEquilibrada() const { return equilibrado; };
-    
-    void inserta(const char &c) {  pila.push(c); }
-    
-    void compruebaEquilibrio() {
-        if (!pila.empty()) {
-            if (pila.top() == ']' || pila.top() == '}' || pila.top() == ')') {
-                char ultimo = pila.top(); pila.pop();
-                
-                if (pila.empty())
-                    equilibrado = false;
-                else if (ultimo == ']' && pila.top() == '[')
-                    pila.pop();
-                else if (ultimo == ')' && pila.top() == '(')
-                    pila.pop();
-                else if (ultimo == '}' && pila.top() == '{')
-                    pila.pop();
-                else
-                    equilibrado = false;
-            }
-        }
-    }
-    
-    void fin() {
-        if (!pila.empty()) equilibrado = false;
-    }
-    
-};
+
+bool compruebaEquilibrio(std::stack<char> &pila) {
+	if (!pila.empty()) {
+		if (pila.top() == ']' || pila.top() == '}' || pila.top() == ')') {
+			char ultimo = pila.top(); pila.pop();
+
+			if (pila.empty())
+				return false;
+			else if (ultimo == ']' && pila.top() == '[')
+				pila.pop();
+			else if (ultimo == ')' && pila.top() == '(')
+				pila.pop();
+			else if (ultimo == '}' && pila.top() == '{')
+				pila.pop();
+			else
+				return false;
+		}
+	}
+
+	return true;
+}
 
 
 bool resuelveCaso() {
-    std::string entrada;
-    ParentesisEquilibrados p;
-    
-    getline(std::cin, entrada);
-    
-    if (!std::cin) return false;
-    
-    char tmp;
-    
-    for (int i = 0; i < entrada.size() && p.getEquilibrada(); ++i) {
-        tmp = entrada[i];
-        if (tmp == '[' || tmp == ']' || tmp == '(' || tmp == ')' || tmp == '{' || tmp == '}') {
-            p.inserta(tmp);
-            p.compruebaEquilibrio();
-        }
-    }
-    
-    if (p.getEquilibrada()) p.fin();
-    
-    if (p.getEquilibrada()) std::cout << "SI\n";
-    else std::cout << "NO\n";
-    
-    return true;
+	std::string entrada;
+
+	getline(std::cin, entrada);
+
+	if (!std::cin) return false;
+
+	std::stack<char> pila;
+	bool continua = true;
+
+	for (int i = 0; i < entrada.size() && continua; ++i) {
+		if(entrada[i] == '[' || entrada[i] == '(' || entrada[i] == '{')
+			pila.push(entrada[i]);
+		if (entrada[i] == ']' || entrada[i] == ')' || entrada[i] == '}') {
+			pila.push(entrada[i]);
+			continua = compruebaEquilibrio(pila);
+		}
+	}
+
+	if (continua && pila.empty()) std::cout << "SI\n";
+	else std::cout << "NO\n";
+
+	return true;
 }
 
 int main() {
-    
-    // Para la entrada por fichero.
-    // Comentar para acepta el reto
+
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
 #ifndef DOMJUDGE
-    std::ifstream in("datos.txt");
-    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
 #endif
-    
-    while(resuelveCaso());
-    
-    // Para restablecer entrada. Comentar para acepta el reto
+
+	while (resuelveCaso());
+
+	// Para restablecer entrada. Comentar para acepta el reto
 #ifndef DOMJUDGE // para dejar todo como estaba al principio
-    std::cin.rdbuf(cinbuf);
-    //system("PAUSE");
+	std::cin.rdbuf(cinbuf);
+	//system("PAUSE");
 #endif
-    
+
 }
